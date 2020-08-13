7e561aac16a1c313555331f0ecf9602b7d292c8b
Duke09 <debopriyo09@outlook.com>
2020-08-13 19:27:01 +0530

    CBV for votes added results and vote view and templates

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls import url
 
-from .views import index, detail, IndexView, DetailView, DeleteView
+from .views import ResultsView, VoteView, index, detail, IndexView, DetailView, DeleteView
 
 app_name = 'polls'
 
@@ -10,7 +10,8 @@ urlpatterns = [
     # path('polls/<int:question_id>/', detail, name='detail'),
     path('polls/', IndexView.as_view(), name='index'),
     path('polls/<int:pk>/', DetailView.as_view(), name='detail'),
-    
+    path('polls/vote/<int:pk>/', VoteView.as_view(), name='vote'),
+    path('polls/results/<int:pk>/', ResultsView.as_view(), name='results'),
     # using regular expression
     url(r'^$', IndexView.as_view(), name='polls_index'),
     url(r'^(?P<pk>[0-9]+)/$', DetailView.as_view(), name='polls_detail'),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import Http404
-from django.views.generic import ListView, DetailView, DeleteView
+from django.shortcuts import get_object_or_404, redirect, render
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import View, ListView, DetailView, DeleteView
+from django.views.generic.base import TemplateResponseMixin
 
 from .models import Question, Choice
 from .mixins import RequireLoginMixin
@@ -17,6 +19,18 @@ def detail(request, question_id):
         raise Http404("Question Does Not Exist")
     return render(request, 'polls/detail.html', {'question': question})
 
+def vote(request, question_id):
+    if request.method == 'POST':
+        question = get_object_or_404(Question, pk=question_id)
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST.get['choice'])
+        except(KeyError, Choice.DoesNotExist):
+            return render(request, 'polls?detail.html', {'question': question})
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
 # Class Based Views
 class IndexView(RequireLoginMixin, ListView):
     template_name = 'polls/index.html'
@@ -28,3 +42,31 @@ class IndexView(RequireLoginMixin, ListView):
 class DetailView(DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+
+class VoteView(View):
+    def get_queryset(self, choice_id):
+        return Choice.objects.get(pk=choice_id)
+
+    def post(self, request, pk):
+        question_id = pk
+        choice_id = request.POST.get('choice', None)
+        
+        try:
+            queryset = self.get_queryset(choice_id)
+        except(KeyError, Choice.DoesNotExist):
+            return redirect('polls:detail', pk=question_id)
+        else:
+            queryset.votes += 1
+            queryset.save()
+            return redirect('polls:results', pk=question_id)
+
+class ResultsView(TemplateResponseMixin, View):
+
+    def get_queryset(self, question_id):
+        return Question.objects.get(pk=question_id)
+
+    def get(self, request, pk):
+        queryset = self.get_queryset(pk)
+        context = {'question': queryset}
+        return render(request, 'polls/results.html', context)
