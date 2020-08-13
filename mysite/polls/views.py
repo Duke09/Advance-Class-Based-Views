c6a7a228b79a3073da318467a0317344c238c269
Duke09 <debopriyo09@outlook.com>
2020-08-13 17:06:09 +0530

    Created Question and Choice model, created views for displaying question and added url patterns and html files

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import Http404
+
+from .models import Question, Choice
 
 # Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question Does Not Exist")
+    return render(request, 'polls/detail.html', {'question': question})
