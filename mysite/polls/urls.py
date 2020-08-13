40609df08d7777dbc349f5f2edfda4d6f1d21ac1
Duke09 <debopriyo09@outlook.com>
2020-08-13 17:30:27 +0530

    Changing from function based view to class based view

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
+from django.conf.urls import url
 
-from .views import index, detail
+from .views import index, detail, IndexView, DetailView, DeleteView
 
 app_name = 'polls'
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('polls/<int:question_id>/', detail, name='detail'),
+    # path('', index, name='index'),
+    # path('polls/<int:question_id>/', detail, name='detail'),
+    path('polls/', IndexView.as_view(), name='index'),
+    path('polls/<int:pk>/', DetailView.as_view(), name='detail'),
+    
+    # using regular expression
+    url(r'^$', IndexView.as_view(), name='polls_index'),
+    url(r'^(?P<pk>[0-9]+)/$', DetailView.as_view(), name='polls_detail'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.views.generic import ListView, DetailView, DeleteView
 
 from .models import Question, Choice
 
@@ -14,3 +15,15 @@ def detail(request, question_id):
     except Question.DoesNotExist:
         raise Http404("Question Does Not Exist")
     return render(request, 'polls/detail.html', {'question': question})
+
+# Class Based Views
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
