58e3f60d3c7ad6c25c90682ef9c14b6c3a47d138
Duke09 <debopriyo09@outlook.com>
2020-08-14 10:55:00 +0530

    Authentication

--- mysite/polls/api/urls.py
+++ mysite/polls/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework import urlpatterns
-from .views import QuestionViewSet, ChoiceViewSet
+from .views import CustomQuestionView, QuestionViewSet, ChoiceViewSet
 
 router = routers.DefaultRouter()
 router.register('question', QuestionViewSet)
 router.register('choice', ChoiceViewSet)
+router.register('custom_question', CustomQuestionView, basename='poll')
 
 urlpatterns = [
     path('', include(router.urls)),
--- mysite/polls/api/views.py
+++ mysite/polls/api/views.py
@@ -1,13 +1,23 @@
 from rest_framework import viewsets
 from rest_framework import serializers
+from rest_framework import authentication
+from rest_framework.response import Response
 
 from ..models import Question, Choice
 from .serializers import QuestionSerializer, ChoiceSerializer
+from .authenticate import AdminOnlyAuth
 
 class QuestionViewSet(viewsets.ModelViewSet):
+    # authentication_classes = (AdminOnlyAuth,)
     queryset =  Question.objects.all().order_by('-pub_date')
     serializer_class = QuestionSerializer
 
 class ChoiceViewSet(viewsets.ModelViewSet):
     queryset = Choice.objects.all()
     serializer_class = ChoiceSerializer
+
+class CustomQuestionView(viewsets.ViewSet):
+    def list(self, request, format=None):
+        questions = [question.question_text for question in Question.objects.all()]
+        # choices = [choice.choice_text for choice in Choice.objects.filter(question__in=[question.id for question in Question.objects.all()])]
+        return Response(questions)
