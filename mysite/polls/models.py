b35c3210c5e4c0a749c1164825c10cec75ce4e52
Duke09 <debopriyo09@outlook.com>
2020-08-17 21:05:48 +0530

    created test for models and views and generated coverage file

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -8,6 +8,12 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def question_and_date(self):
+        return "{} posted at {}".format(
+            self.question_text,
+            self.pub_date
+        )
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=120)
@@ -15,3 +21,7 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+    
+    def add_vote(self):
+        self.votes += 1
+        self.save()
