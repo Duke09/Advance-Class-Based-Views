aba0201815cb3442c221dc6b424311b88fb952b5
Duke09 <debopriyo09@outlook.com>
2020-08-18 08:51:37 +0530

    Fixed bug for test_pos_success and test_post_failure

--- mysite/polls/tests/test_views.py
+++ mysite/polls/tests/test_views.py
@@ -1,10 +1,53 @@
-from django.test import TestCase, RequestFactory
+from django.test import TestCase, RequestFactory, Client
+
 from django.urls import reverse
 
 from model_mommy import mommy
 
 from ..views import VoteView
 from ..models import Choice
+
+class TestVoteViewIntegration(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.question_model = mommy.make('polls.Question')
+        self.choice_models = mommy.make(
+            'polls.Choice',
+            question=self.question_model,
+            _quantity=3
+        )
+
+    def test_post_success(self):
+        choice = self.choice_models[0]
+        response = self.client.post(
+            reverse(
+                'polls:vote',
+                kwargs={
+                    'pk': choice.question.id
+                }
+            ),
+            {
+                'choice': choice.id
+            },
+            follow=True
+        )
+        self.assertIn(302, response.redirect_chain[0])
+        self.assertTemplateUsed(response, 'polls/results.html')
+
+    def test_post_failure(self):
+        choice = self.choice_models[0]
+        response = self.client.post(
+            reverse(
+                'polls:vote',
+                kwargs={
+                    'pk': choice.question.id
+                }
+            ),
+            follow=True
+        )
+        self.assertIn(302, response.redirect_chain[0])
+        self.assertTemplateUsed(response, 'polls/detail.html')
+
 
 class TestVote(TestCase):
     def setUp(self):
